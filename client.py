803a403b0f9b2edc4c8789f2d7aaef0c0b6ef1d2
Yuxuan Zhou <32389418+ZhouYuxuan97@users.noreply.github.com>
2024-05-23 02:52:16 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 from datetime import datetime
 import argparse
+import sys
 
 def send_timestamp(server_ip, server_port, note):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,10 +12,14 @@ def send_timestamp(server_ip, server_port, note):
     client_socket.close()
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Send note with timestamp to the server.')
-    parser.add_argument('server_ip', type=str, help='IP address of the server')
-    parser.add_argument('server_port', type=int, help='Port of the server')
-    parser.add_argument('note', type=str, help='Note to send')
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser(description='Send note with timestamp to the server.')
+    # parser.add_argument('server_ip', type=str, help='IP address of the server')
+    # parser.add_argument('server_port', type=int, help='Port of the server')
+    # parser.add_argument('note', type=str, help='Note to send')
+    # args = parser.parse_args()
 
-    send_timestamp(args.server_ip, args.server_port, args.note)
+    ip = sys.argv[1]
+    port = int(sys.argv[2])
+    note = sys.argv[3]
+    # send_timestamp(args.server_ip, args.server_port, args.note)
+    send_timestamp(ip, port, note)
